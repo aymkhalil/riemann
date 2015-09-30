ce348bf2587d7dda233c969f395545351fb1c09d
Ayman Khalil <aymkhalil85@gmail.com>
2015-09-30 16:08:51 +0300

    Redirect cython binaries to the current directory

--- src/setup_cy.py
+++ src/setup_cy.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
+extensions=[Extension("euler_1D_cy",["euler_1D_cy.pyx"])]
+
 setup(
-    ext_modules = cythonize("euler_1D_cy.pyx"),
+    ext_modules = cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
